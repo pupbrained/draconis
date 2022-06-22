e7e3a2a9e1738a5a7668edf81a9c2ccf2a465975
marsupialgutz <mars@possums.xyz>
2022-06-22 17:16:44 -0400

    idek what i   oh yeah

--- flake.nix
+++ flake.nix
@@ -5,26 +5,31 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
-        defaultPackage = naersk-lib.buildPackage {
-          root = ./.;
-          buildInputs = with pkgs; [ dbus ];
-          nativeBuildInputs = with pkgs; [ pkg-config ];
-        };
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    naersk,
+  }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      naersk-lib = pkgs.callPackage naersk {};
+    in {
+      defaultPackage = naersk-lib.buildPackage {
+        root = ./.;
+        buildInputs = with pkgs; [dbus];
+        nativeBuildInputs = with pkgs; [pkg-config];
+      };
 
-        defaultApp = utils.lib.mkApp {
-          drv = self.defaultPackage."${system}";
-        };
+      defaultApp = utils.lib.mkApp {
+        drv = self.defaultPackage."${system}";
+      };
 
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
+      devShell = with pkgs;
+        mkShell {
+          buildInputs = [cargo rustc rustfmt pre-commit rustPackages.clippy dbus];
+          nativeBuildInputs = [pkg-config];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
         };
-      });
+    });
 }
